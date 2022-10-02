f07bd33ba24a32662562d12125f50b36ff565fdd
Bruce Apos <bobapos420@gmail.com>
2022-10-03 03:52:42 +0800

    Setup project

---
+++ components/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div>
+      <h1 className=" text-9xl font-bold">
+        Bob<span className=" font-light">devs</span>
+      </h1>
+    </div>
+  );
+};
+
+export default Header;
---
+++ components/Meta.js
@@ -0,0 +1,21 @@
+import Head from "next/head";
+
+const Meta = ({ title, keywords, description }) => {
+  return (
+    <Head>
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <meta name="keywords" content={keywords} />
+      <meta name="description" content={description} />
+      <meta charSet="utf-8" />
+      <title>{title}</title>
+    </Head>
+  );
+};
+
+Meta.defaultProps = {
+  title: "Bob devs",
+  keywords: "Developer, web dev, flutter, javascript, reactjs, nextjs",
+  description: "Bob can develop websites and apps.",
+};
+
+export default Meta;
