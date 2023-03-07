41228445967d19d58f848905fe5c72b9d4cb6691
Bruce Apos <bobapos420@gmail.com>
2023-03-08 02:46:47 +0800

    Add links

--- components/Footer.js
+++ components/Footer.js
@@ -1,15 +1,24 @@
 import React from "react";
 import Image from "next/image";
+import Link from "next/link";
 
 const Footer = () => {
   return (
-    <div className="flex flex-row gap-10 mt-14 pb-10">
-      <div className="">
-        <Image src="/linkedin-logo.png" width="50" height="50" />
-      </div>
-      <div className="">
-        <Image src="/github-logo.png" width="50" height="50" />
-      </div>
+    <div className="flex flex-row gap-10 pb-10 mt-14">
+      <Link
+        href="https://www.linkedin.com/in/bruce-oliver-apos-51b3381b3/"
+        passHref={true}
+      >
+        <a target="_blank">
+          <Image src="/linkedin-logo.png" width="50" height="50" />
+        </a>
+      </Link>
+
+      <Link href="https://github.com/boapos" passHref={true}>
+        <a target="_blank">
+          <Image src="/github-logo.png" width="50" height="50" />
+        </a>
+      </Link>
     </div>
   );
 };
