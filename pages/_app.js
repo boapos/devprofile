9e99ec49f986a511403e6e4c696b585a8d159246
Bruce Apos <bobapos420@gmail.com>
2023-03-10 06:34:45 +0800

    Add web font loader

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,19 @@
 import Layout from "../components/Layout";
 import "../styles/globals.css";
+import { useEffect } from "react";
 
 function MyApp({ Component, pageProps }) {
+  useEffect(() => {
+    (async () => {
+      const WebFont = await import("webfontloader");
+      WebFont.load({
+        custom: {
+          families: ["Poppins", "Roboto Slab"],
+        },
+      });
+    })();
+  }, []);
+
   return (
     <>
       <Layout>
